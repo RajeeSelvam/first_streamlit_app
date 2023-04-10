83e16bda0d8af3261f738edcecab3278587c2b35
RajeeSelvam <130377069+RajeeSelvam@users.noreply.github.com>
2023-04-10 19:53:08 +0530

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 
 streamlit.text("Apple and oatmeal")
+
+streamlit.text("Orange and bread")
+
+streamlit.text("Sandwich and Smootie")
